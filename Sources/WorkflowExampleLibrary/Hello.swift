79cfad0fc8e2c2c051642d5c724d6bdd2769097e
Stefan Springer <springer@stefanspringer.com>
2022-08-11 11:32:29 +0200

    using SwiftWorkflow >= 0.0.19

--- Sources/WorkflowExampleLibrary/Hello.swift
+++ Sources/WorkflowExampleLibrary/Hello.swift
@@ -41,7 +41,9 @@ struct Hello_stepData: StepData {
     // Add non-static members of type XMessage here for all messages.
     // Only non-static members will be collected for the overview!
     
-    public let sayingHello = Message(id: "saying hello", type: .Info, info: [
-        .en: "saying hello to $1"
-    ])
+    public let sayingHello = Message(id: "saying hello", type: .Info,
+        fact: [
+            .en: "saying hello to $1"
+        ]
+    )
 }
